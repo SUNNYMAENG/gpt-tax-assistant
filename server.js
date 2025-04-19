248146b9f6473e3deca40eb67f30250cb1e234ec
SUNNYMAENG <asoprompt@gmail.com>
2025-04-19 16:27:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { supabase } = require('./utils/supabase');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
